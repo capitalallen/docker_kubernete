ef737a77eb04c69f719c60d118d3ad7b8b088553
Zeyu Zhang <zzhan785@uwo.ca>
2021-03-28 00:15:57 -0400

    change port

--- rebuild_image.sh
+++ rebuild_image.sh
@@ -2,4 +2,4 @@ docker container rm node-web-app
 
 docker build -t capitalallen/node-web-app .
 
-docker run -p 8080:8080 -d capitalallen/node-web-app
+docker run -p 5000:5000 -d capitalallen/node-web-app
